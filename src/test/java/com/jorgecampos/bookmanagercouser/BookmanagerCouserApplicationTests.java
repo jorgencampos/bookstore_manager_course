5fff2592a4bf29e6c69c275db1e733412f54c277
Jorge Brandão de Campos <jorge.campos@dbccompany.com.br>
2023-08-24 11:04:04 -0300

    First commit: project structor creater

---
+++ src/test/java/com/jorgecampos/bookmanagercouser/BookmanagerCouserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgecampos.bookmanagercouser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanagerCouserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
